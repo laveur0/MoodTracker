bc73b3e41fca9970e627933e8fe105a764a68a98
laveur0 <ac.noumsi@gmail.com>
2018-07-09 05:57:21 +0100

    Create project moodTracker and two empty activities (MainActivity, MoodHistoryActivity)

---
+++ app/src/main/java/com/noumsi/christian/moodtracker/controller/MainActivity.java
@@ -0,0 +1,15 @@
+package com.noumsi.christian.moodtracker.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.noumsi.christian.moodtracker.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/noumsi/christian/moodtracker/controller/MoodHistoryActivity.java
@@ -0,0 +1,15 @@
+package com.noumsi.christian.moodtracker.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.noumsi.christian.moodtracker.R;
+
+public class MoodHistoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mood_history);
+    }
+}
